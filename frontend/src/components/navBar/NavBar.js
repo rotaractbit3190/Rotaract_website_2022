5d480bdf15b0999a78b34ab889fa84d12a48feb1
rotaractbit3190 <72608504+rotaractbit3190@users.noreply.github.com>
2024-01-18 19:14:58 +0000

    modified:   src/components/Board/Board.css 	modified:   src/components/navBar/NavBar.js

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -1,16 +1,16 @@
-import "./nav.css";
-
-import { Link,useNavigate  } from "react-router-dom";
-import logo from "./logo.png";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FiMenu } from "react-icons/fi";
-import thelogo from "./logorotaract.png";
-import Divider from "../Homepage/divider/Divider";
 import { BsInstagram, BsTwitter, BsLinkedin } from "react-icons/bs";
 import { GoMail } from "react-icons/go";
+import logo from "./logo.png";
+import thelogo from "./logorotaract.png";
+import Divider from "../Homepage/divider/Divider";
+import "./nav.css";
+
 export default function NavBar(props) {
-  const [sidebar, setsidebar] = useState(false);
-  let navigate=useNavigate()
+  const [sidebar, setSidebar] = useState(false);
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -18,23 +18,48 @@ export default function NavBar(props) {
   };
 
   const handleClick = () => {
-    setsidebar(true);
+    setSidebar(true);
     props.showcursor(false);
   };
+
   const gotop = () => {
-    setsidebar(false);
-    window.reload();
+    setSidebar(false);
+    window.location.reload();
   };
+
   const closeClick = () => {
-    setsidebar(false);
+    setSidebar(false);
     props.showcursor(true);
   };
+
+  useEffect(() => {
+    let prevScrollPos = window.pageYOffset;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        document.querySelector(".header").style.top = "0";
+      } else {
+        document.querySelector(".header").style.top = "-65px"; // Adjust this value to match your navbar height
+      }
+
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Empty dependency array ensures the effect runs only once after the initial render
+
   return (
     <nav>
       <div className="header">
         <div className="innerheader">
           <div className="logo-image">
-            <img src={logo} alt="here you go" className="logoSize " />
+            <img src={logo} alt="here you go" className="logoSize" />
           </div>
           <div className="burger-name">
             <FiMenu size="2rem" className="try" onClick={handleClick} />
@@ -52,9 +77,11 @@ export default function NavBar(props) {
             <li>
               <Link to="/ContactUs">Contact</Link>
             </li>
-            {localStorage.getItem('token')?<li>
-                  <button onClick={handleLogout}>Logout</button>
-                </li>:null}
+            {localStorage.getItem("token") ? (
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            ) : null}
             <li>
               <a href="https://forms.gle/rwjZh4pCsaQXLD8r9">Join Us</a>
             </li>
@@ -91,20 +118,31 @@ export default function NavBar(props) {
                 </Link>
               </li>
               <li>
-                <a href="https://forms.gle/MzbJ6ZP2XySagU1E8" className="header-li" onClick={gotop}>
+                <a
+                  href="https://forms.gle/MzbJ6ZP2XySagU1E8"
+                  className="header-li"
+                  onClick={gotop}
+                >
                   Join Us
                 </a>
               </li>
               <div className="sociala">
-                <a href="https://www.instagram.com/rotaractclubofbit/" target="blank"> <BsInstagram color="white" size={"30px"} className="spacing" /></a>
-                <a href="https://twitter.com/RotaractBIT"><BsTwitter color="white" size={"30px"} className="spacing" /></a>
+                <a href="https://www.instagram.com/rotaractclubofbit/" target="blank">
+                  {" "}
+                  <BsInstagram color="white" size={"30px"} className="spacing" />
+                </a>
+                <a href="https://twitter.com/RotaractBIT">
+                  <BsTwitter color="white" size={"30px"} className="spacing" />
+                </a>
                 <a href="https://www.linkedin.com/company/rotaractclubofbit/mycompany/">
-                  
-                <BsLinkedin color="white" size={"30px"} className="spacing" /></a>
-               
-                <a href="mailto:bitrotaract@gmail.com"> <GoMail color="white" size={"36px"} className="spacing" /></a>
-               
-              </div>
+                  <BsLinkedin color="white" size={"30px"} className="spacing" />
+                </a>
+
+                <a href="mailto:bitrotaract@gmail.com">
+                  {" "}
+                  <GoMail color="white" size={"36px"} className="spacing" />
+                </a>
+                </div>
             </div>
           </div>
         ) : null}
