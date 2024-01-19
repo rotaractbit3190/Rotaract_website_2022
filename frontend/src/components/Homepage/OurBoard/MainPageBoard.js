12d5e773506162ea1c3c780b2e7ab5bd8ef96ffe
rotaractbit3190 <72608504+rotaractbit3190@users.noreply.github.com>
2024-01-19 10:14:53 +0000

    modified:   src/components/Homepage/OurBoard/MainPageBoard.css 	modified:   src/components/Homepage/OurBoard/MainPageBoard.js 	modified:   src/components/Homepage/OurBoard/image-2.jpg 	modified:   src/components/Homepage/OurBoard/image-3.jpg 	modified:   src/components/Homepage/OurBoard/image-4.jpg 	modified:   src/components/Homepage/OurBoard/image-5.jpg 	modified:   src/components/Homepage/OurBoard/image-6.jpg 	modified:   src/components/Homepage/OurBoard/image.jpeg 	modified:   src/components/Homepage/OurBoard/imgx.jpg

--- frontend/src/components/Homepage/OurBoard/MainPageBoard.js
+++ frontend/src/components/Homepage/OurBoard/MainPageBoard.js
@@ -74,28 +74,25 @@ export default function MainPageBoard() {
         <center>
           <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="2000">
-              <img src={imagex} class="d-block w-50" alt="..." height={600} />
+              <img src={imagex} class="d-block w-10" alt="..." height={600} />
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-              <img src={image2} class="d-block w-50" alt="..." height={600} />
-            </div>
-            <div class="carousel-item" data-bs-interval="2000">
-              <img src={image3} class="d-block w-50" alt="..." height={600} />
+              <img src={image2} class="d-block .w-10" alt="..." height={600} />
             </div>
             <div class="carousel-item " data-bs-interval="2000">
-              <img src={res} class="d-block w-50" alt="..." height={600} />
+              <img src={image3} class="d-block w-10" alt="..." height={600} />
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-              <img src={image3} class="d-block w-50" alt="..." height={600} />
+              <img src={image4} class="d-block w-10" alt="..." height={600} />
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-              <img src={image4} class="d-block w-50" alt="..." height={600} />
+              <img src={image5} class="d-block w-10" alt="..." height={600} />
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-              <img src={image5} class="d-block w-50" alt="..." height={600} />
+              <img src={image6} class="d-block w-10" alt="..." height={600} />
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-              <img src={image6} class="d-block w-50" alt="..." height={600} />
+              <img src={res} class="d-block w-10" alt="..." height={600} />
             </div>
           </div>
         </center>
