7771885a30b5aefa2c81f94bcfe49641dec51ea5
rotaractbit3190 <72608504+rotaractbit3190@users.noreply.github.com>
2024-03-03 07:13:26 +0000

    modified:   .firebase/hosting.YnVpbGQ.cache 	modified:   src/components/navBar/Magazine.js

--- frontend/src/components/navBar/Magazine.js
+++ frontend/src/components/navBar/Magazine.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Magazine() {
   return (
-    <div style={{ marginTop: "200px", paddingTop: "73px", width: "80%", margin: "0 auto", borderRadius: "10px"}}> {/* Adjusted marginTop to 150px */}
+    <div style={{ marginTop: "4.5%", paddingTop: "4.7%", width: "80%", margin: "0 auto", borderRadius: "10px"}}> {/* Adjusted marginTop to 150px */}
       <iframe
         title="VRINDA Magazine"
         src="https://heyzine.com/flip-book/a6b9597e87.html"
